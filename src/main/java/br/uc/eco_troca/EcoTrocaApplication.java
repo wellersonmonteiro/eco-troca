1c866152ec59f6e6818492419354a55c06d3556c
wellersonmonteiro <010102well@gmail.com>
2025-05-21 22:21:49 -0300

    frist commit

---
+++ src/main/java/br/uc/eco_troca/EcoTrocaApplication.java
@@ -0,0 +1,13 @@
+package br.uc.eco_troca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoTrocaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoTrocaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/uc/eco_troca/EcoTrocaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.uc.eco_troca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoTrocaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
